37be4047083198b005b9510e49d083091449f7b0
sadegh paksima <spaxima1@gmail.com>
2022-05-08 02:33:51 +0430

    conect front to back  add number input in to form fot mony create table for  ids in mongodb

--- app.py
+++ app.py
@@ -20,3 +20,30 @@ def get_database():
 
     return client['StripeTables']
 
+db=get_database()
+#create or connect  Table for  ids payment
+idsTable=db['idsTable']
+
+
+#connect to stripe web site and payment 
+@app.route('/charge', methods=['POST'])
+def charge():
+
+    api_key = 'sk_test_51Kux0CJJ4eit2YqKLw6BGayeA4eHnQi8FqNwieOuDmA7QLNuP7KH482k1oqmQwN8D7Mh6E5MpMpg3nL1VXrGd20J00qxt0GavI'
+    token = request.form.get('stripeToken')
+    Mony=request.form.get("PaymentMoney")
+    
+    headers = {'Authorization' : f'Bearer {api_key}'}
+    data = {
+            'amount' : Mony, 
+            'currency' : 'usd', 
+            'description' : 'Another Charge', 
+            'source' : token
+        }
+    
+    #send post request to stripe web site 
+    requestStripe = requests.post('https://api.stripe.com/v1/charges', headers=headers, data=data)
+    idPayment={"id":requestStripe.json()['id']}
+    idsTable.insert_one(idPayment)
+
+    return jsonify(requestStripe.json())
