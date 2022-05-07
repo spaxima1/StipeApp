d85bd9f52d1ecc6c5a70dcbd344190282115e99e
sadegh paksima <spaxima1@gmail.com>
2022-05-08 01:39:34 +0430

    creat  function for create and connect to mongodb databse

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template, request,jsonify
 import requests 
 
+#create app for run server 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+#create root rout 
 @app.route('/')
 def index():
+    #render html file in template folder
     return render_template('index.html')
+
+#create or Connect to DataBase 
+def get_database():
+    from pymongo import MongoClient
+
+    CONNECTION_STRING = 'mongodb://127.0.0.1:27017/'
+    client = MongoClient(CONNECTION_STRING)
+
+    return client['StripeTables']
+
